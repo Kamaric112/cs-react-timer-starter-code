bfa6b9b9eb4b7e483be8dbfb2edde13b558b8390
Kamaric112 <ikasuzu123@gmail.com>
2022-09-19 21:35:24 +0700

    updated GetHours

--- src/formatTime.js
+++ src/formatTime.js
@@ -2,7 +2,7 @@ export const formatTime = (time) => {
   const getSeconds = `0${time % 60}`.slice(-2);
   const minutes = `${Math.floor(time/60)}`;
   const getMinutes = `0${minutes % 60}`.slice(-2);
-  const getHours = `${Math.floor(time/3600)}`.slice(-2);
+  const getHours = `${Math.floor(minutes/60)}`.slice(-2);
 
   return `${getHours} : ${getMinutes} : ${getSeconds}`;
 };
